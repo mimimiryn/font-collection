04d88051fae35da24deeec63d0d776555ef519be
mimimiryn <mimimiryn@gmail.com>
2015-06-18 10:41:09 +0900

    Initial Commit

---
+++ fontcollect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fontcollect
+//
+//  Created by miryon on 2015/06/18.
+//  Copyright (c) 2015年 miryon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
