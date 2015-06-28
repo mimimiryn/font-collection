b4a1ed9eb007a5f571bb564af91ed315ba4d6d36
mimimiryn <mimimiryn@gmail.com>
2015-06-28 16:44:24 +0900

    はい

--- fontcollect/kitaeruViewController.swift
+++ fontcollect/kitaeruViewController.swift
@@ -11,11 +11,14 @@ import UIKit
 class kitaeruViewController: UIViewController {
     
     @IBOutlet var myLabel2: UILabel!
-    @IBOutlet var choiceButtons: UIButton!
+    @IBOutlet var choiceButton: Array<UIButton> = []
+    //@IBOutlet var choiceButton: UIButton!
+    //@IBOutlet var choiceButton: UIButton!
     
+    var nameArray = ["A-OTF 見出ゴMB1 Std DeBold","A-OTF-A1 明朝Std-Bold","おめかし","round-mplus-1p-thin","えるまー","さむらい"]
     
-    var choiceButtons = ["A-OTF 見出ゴMB1 Std DeBold","A-OTF-A1 明朝Std-Bold","おめかし","round-mplus-1p-thin","えるまー","さむらい"]
     var fontArray = ["A1MinchoStd-Bold", "MiGoMB1Std-DeBold", "Omekashi-Font", "rounded-mplus-1p-thin", "ElmerFont", "Samurai"]
+    
     var font: UIFont!
     
     
@@ -27,14 +30,33 @@ class kitaeruViewController: UIViewController {
         
         
         myLabel2.text = "あ"
+        
+        //正解フォント選ぶ
         var nameNumber = Int(arc4random_uniform(UInt32(fontArray.count)))
+        
+        //そのフォントで文字を書く
         myLabel2.font = UIFont(name:fontArray[nameNumber], size: 130)
         
-        for var i = 0; i < choiceButtons.count; i++ {
-            choiceButtons[i].setTitle(quizArray[random][i+1] as! NSString as String,forState: .Normal)
         
         
+        //        correctNumber.setTitle("choiceButton[correctNumber]", forState: UIControlState.Normal) =
+        //            nameNumber.setTitle("nameArray[nameNumber]", forState: UIControlState.Normal)
+        
+        //choiceButton[correctNumber] = nameArray[nameNumber]
+        
+        var nameArrray2 = nameArray
+        nameArrray2.removeAtIndex(nameNumber)
+        for var i = 0; i < 3; i++ {
+            var randomChoice = Int(arc4random_uniform(UInt32(nameArrray2.count)))
+            choiceButton[i].setTitle(nameArrray2[randomChoice] as NSString as String,forState: .Normal)
+            
+            nameArrray2.removeAtIndex(randomChoice)
         }
+        
+        //何番目ボタンに正解を入れるか
+        var correctNumber = Int(arc4random_uniform(UInt32(2)))
+
+        choiceButton[correctNumber].setTitle(nameArray[nameNumber] as NSString as String, forState: .Normal)
     }
     
     
